92b0ca93a0547972ec9e8103f3d57ce3caed32c5
Liviu Ionescu <ilg@livius.net>
2024-10-15 03:33:47 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -42,7 +42,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout 
-      title={`Welcome to flex`}
+      title="Welcome!"
       description="A binary package with the flex executables">
       <HomepageHeader />
       <main>
